19ff16ea5f19045f20ba46cb3a21ba69f89e69dc
angelyintt <75771210+angelyintt@users.noreply.github.com>
2020-12-15 21:19:24 -0800

    test

--- R copy/prime.R
+++ R copy/prime.R
@@ -8,8 +8,8 @@
 #' @examples
 #' prime(100)
 #' Find out all prime numbers from 1 to 100
-library(primes)
 prime = function(n){
+  library(primes)
   temp=c()
   for (i in 1:n) {
     if(is_prime(i)==TRUE){
--- shuting/R/prime.R
+++ shuting/R/prime.R
@@ -1,5 +1,5 @@
 #' Find out all prime numbers from 1 to N
-#' @import primes
+#'
 #' @param n The last number to be tested
 #'
 #' @return Return a list of prime numbers from 1 to N
@@ -8,11 +8,11 @@
 #' @examples
 #' prime(100)
 #' Find out all prime numbers from 1 to 100
-usethis::use_package("prime")
 prime = function(n){
+  library(primes)
   temp=c()
   for (i in 1:n) {
-    if(primes::is_prime(i)==TRUE){
+    if(is_prime(i)==TRUE){
       temp=c(temp, i)
     }else{
       temp=c(temp)
